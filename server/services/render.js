b4474ca8f671ca9ccbf8236ea656f2fa0b90bf2c
Aman <patilaman080@gmail.com>
2020-11-14 17:18:20 +0530

    env var3

--- server/services/render.js
+++ server/services/render.js
@@ -2,7 +2,7 @@ const axios=require('axios')
 
 exports.homeRoute=(req,res)=>{
     // get request to api/users
-    axios.get('http://localhost:3000/api/users')
+    axios.get(`http://localhost:${process.env.PORT}/api/users`)
     .then(function (response){
     
         res.render('index',{
@@ -22,7 +22,7 @@ exports.addUser=(req,res)=>{
 }
 
 exports.updateUser=(req,res)=>{
-    axios.get('http://localhost:3000/api/users',{params:{id:req.query.id}})
+    axios.get(`http://localhost:${process.env.PORT}/api/users`,{params:{id:req.query.id}})
     .then(function(userdata){
         res.render('update_user',{
             user:userdata.data
